f4d86fe84e7279ce29d799bbd3a779d8315a069b
Reeba Sayed <43046453+rssayed@users.noreply.github.com>
2020-06-21 21:28:39 -0500

    Update FuelQuoteHistoryForm.js

--- src/components/FuelQuoteHistoryForm.js
+++ src/components/FuelQuoteHistoryForm.js
@@ -7,21 +7,21 @@ class FuelQuoteHistoryForm extends React.Component {
       return [
          {
             'gallons': 10,
-            'shipping address': '301 Fannin St. Houston, Texas 77002',
+            'delivery address': '301 Fannin St. Houston, Texas 77002',
             'order date': '08/05/2020',
             'price per gallon':'2',
             'total cost':'20'
          },
          {
             'gallons': 24,
-            'shipping address': '301 Fannin St. Houston, Texas 77002',
+            'delivery address': '301 Fannin St. Houston, Texas 77002',
             'order date': '05/07/2019',
             'price per gallon':'1',
             'total cost':'24'
          },
          {
             'gallons': 7,
-            'shipping address': '301 Fannin St. Houston, Texas 77002',
+            'delivery address': '301 Fannin St. Houston, Texas 77002',
             'order date': '10/06/2018',
             'price per gallon':'10',
             'total cost':'70'
@@ -35,8 +35,8 @@ class FuelQuoteHistoryForm extends React.Component {
          Order History
             <table>
                <tr>
-                  <th scope="col">Order Date</th>
-                  <th scope="col">Shipping Address</th>
+                  <th scope="col">Delivery Date</th>
+                  <th scope="col">Delivery Address</th>
                   <th scope="col">Gallons</th>
                   <th scope="col">Price Per Gallon</th>
                   <th scope="col">Total Cost</th>
@@ -44,7 +44,7 @@ class FuelQuoteHistoryForm extends React.Component {
                {this.getUserOrderHistory().map((order) => (
                   <tr>
                      <th scope="row">{order['order date']}</th>
-                     <td>{order['shipping address']}</td>
+                     <td>{order['delivery address']}</td>
                      <td>{order['gallons']}</td>
                      <td>{order['price per gallon']}</td>
                      <td>{order['total cost']}</td>
