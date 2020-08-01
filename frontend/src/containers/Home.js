1f75db794ab5e63487357b1dc0eac10c77bcf450
Reeba Sayed <43046453+rssayed@users.noreply.github.com>
2020-08-01 11:46:05 -0500

    Update Home.js

--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -5,7 +5,6 @@ export default function Home() {
   return (
     <div className="Home">
       <div className="lander">
-        <h1>BMS Fuels</h1>
         <p>Login to access profile. If you are not a member, please register.</p>
       </div>
     </div>
