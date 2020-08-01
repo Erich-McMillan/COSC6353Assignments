2542fd59f752b38493c7aac0dc4df2909f86d0e5
Reeba Sayed <43046453+rssayed@users.noreply.github.com>
2020-08-01 17:58:56 -0500

    Add files via upload

---
+++ frontend/src/containers/FuelHistory.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import {Table} from 'react-bootstrap';
+
+class FuelHistory extends React.Component{
+    
+    getUserOrderHistory() {
+        return [
+           {
+              'delivery address': '301 Fannin St., Houston, TX 77002',
+              'date': '08/05/2020',
+              'gallons': 10,
+              'price per gallon':'2',
+              'total cost':'20'
+           },
+           {
+              'delivery address': '301 Fannin St., Houston, TX 77002',
+              'date': '05/07/2019',
+              'gallons': 24,
+              'price per gallon':'1',
+              'total cost':'24'
+           },
+           {
+              'delivery address': '301 Fannin St., Houston, TX 77002',
+              'date': '10/06/2018',
+              'gallons': 7,
+              'price per gallon':'10',
+              'total cost':'70'
+           }
+        ]
+    }
+ 
+    render(){
+        return (
+            <div className = 'FuelHistory'>
+                <Table bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Delivery Address</th>
+                            <th>Date</th>
+                            <th>Gallons Purchased</th>
+                            <th>Price per gallon</th>
+                            <th>Total Cost</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.getUserOrderHistory().map((order) => 
+                        (
+                            <tr>
+                                <td>{order['delivery address']}</td>
+                                <td>{order['date']}</td>
+                                <td>{order['gallons']}</td>
+                                <td>{order['price per gallon']}</td>
+                                <td>{order['total cost']}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            </div>
+        );
+    }    
+}
+
+export default FuelHistory;
