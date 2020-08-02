2d5c39ae1a70de5725be24b6dd29a3c96eaf58ed
Erich McMillan <erich.mcmillan@hp.com>
2020-08-01 23:02:12 -0500

    Add real quote calculation
    Add quote tests, update pyproject.toml and lock moving dependencies to dev

--- backend/modules/quotes.py
+++ backend/modules/quotes.py
@@ -1,5 +1,6 @@
 from backend.modules.database_helper import i_db_obj, get_database
 from backend.modules.user import i_user
+import backend.modules.user as users
 from backend.modules.profile_management import street_address_from_dict, street_address
 
 class quote(i_db_obj):
@@ -35,12 +36,47 @@ class quote(i_db_obj):
          'delivery_date':self.delivery_date
       }
 
+def get_quote_history(user: i_user):
+   quote_collection = (get_database().appdb)[quote._quote_collection_name]
+   quotes = quote_collection.find({'username' : user.username()})
+   quote_array = []
+   for q in quotes:
+      quote_array.append(quote_from_dict(q).as_dict())
+   return quote_array
+
+def determine_margin(has_history: bool, is_bulk: bool, is_texas: bool) -> float:
+   margin = 0.1
+   
+   if has_history:
+      margin += 0.01
+   if is_texas:
+      margin += 0.02
+   else:
+      margin += 0.04
+   if is_bulk:
+      margin += 0.02
+   else:
+      margin += 0.03
+
+   return margin
+
+def calculate_quote(username: str, numGallons: int, deliveryAddr: street_address) -> str:
+   user = users.get_user(username)
+   has_quote_history = len(get_quote_history(user)) > 0
+   is_texas = deliveryAddr.state.lower() == "TX"
+   bulk_order = numGallons >= 1000
+
+   margin = determine_margin(has_quote_history, bulk_order, is_texas)
+   price = 1.5 * numGallons * margin
+
+   return '{:,.2f}'.format(price)
+
 def quote_from_dict(fromdict: dict)-> quote:
    fuel_quote = quote()
    fuel_quote.username = fromdict['username']
    fuel_quote.gallons_requested = fromdict['gallons_requested']
    fuel_quote.delivery_addr = street_address_from_dict(fromdict['delivery_addr'])
-   fuel_quote.price_per_gallon = fuel_quote.gallons_requested * 10 # needs to use pricing module
+   fuel_quote.price_per_gallon = calculate_quote(fuel_quote.username, fuel_quote.gallons_requested, fuel_quote.delivery_addr) # needs to use pricing module
    fuel_quote.delivery_date = fromdict['delivery_date']
    return fuel_quote
 
@@ -49,11 +85,3 @@ def get_quote(user: i_user, quote_request: dict):
    q = quote_from_dict(quote_request)
    q.save_to_db() #always save the quote whenever made
    return q.as_dict()
-
-def get_quote_history(user: i_user):
-   quote_collection = (get_database().appdb)[quote._quote_collection_name]
-   quotes = quote_collection.find({'username' : user.username()})
-   quote_array = []
-   for q in quotes:
-      quote_array.append(quote_from_dict(q).as_dict())
-   return quote_array
--- backend/tests/test_quotes.py
+++ backend/tests/test_quotes.py
@@ -1,8 +1,10 @@
 import pytest
 
 import backend.modules.quotes as quotes
+import backend.modules.user as user
+import backend.modules.profile_management as profile
 
-def test_quote_from_dict_works():
+def test_quote_from_dict_works(monkeypatch):
    quote_json = {
       'username': 'erich',
       'gallons_requested': 10,
@@ -15,9 +17,14 @@ def test_quote_from_dict_works():
       'delivery_date':'today'
    }
 
+   def mock_calc_quote(*args, **kwargs):
+      return 10
+
+   monkeypatch.setattr(quotes, 'calculate_quote', mock_calc_quote)
+
    final = quotes.quote_from_dict(quote_json)
 
-   assert final.price_per_gallon == 100
+   assert final.price_per_gallon == 10
 
 def test_get_quote_returns_gallons_times_price(monkeypatch):
    quote_json = {
@@ -41,9 +48,14 @@ def test_get_quote_returns_gallons_times_price(monkeypatch):
 
    monkeypatch.setattr(quotes.quote, 'save_to_db', mock_save_db)
 
+   def mock_calc_quote(*args, **kwargs):
+      return 10
+
+   monkeypatch.setattr(quotes, 'calculate_quote', mock_calc_quote)
+
    final_quote = quotes.get_quote(mock_user(), quote_json)
 
-   assert final_quote['price_per_gallon'] == 100
+   assert final_quote['price_per_gallon'] == 10
 
 def test_as_dict_raise_when_gallons_zero():
    q = quotes.quote()
@@ -69,3 +81,36 @@ def test_as_dict_raise_when_pricepergallon_none():
       q.as_dict()
    except:
       pass
+
+def test_get_margin_1percent_bulk_discount_when_over_1000_gallons():
+   base_margin = quotes.determine_margin(False, False, False)
+   bulk_margin = quotes.determine_margin(False, True, False)
+   assert (base_margin - bulk_margin - .01) < .0001
+
+def test_get_margin_2percent_increase_for_out_of_state():
+   base_margin = quotes.determine_margin(False, False, False)
+   bulk_margin = quotes.determine_margin(False, False, True)
+   assert (base_margin - bulk_margin - .02) < .0001
+
+def test_get_margin_1percent_increase_for_repeat_customers():
+   base_margin = quotes.determine_margin(False, False, False)
+   bulk_margin = quotes.determine_margin(True, False, False)
+   assert (base_margin - bulk_margin - .01) < .0001
+
+def test_calculate_quote_truncates_price_at_cent(monkeypatch):
+   def mock_get_user(*args, **kwargs):
+      return None
+
+   def mock_get_quote_hist(*args, **kwargs):
+      return []
+
+   monkeypatch.setattr(user, 'get_user', mock_get_user)
+   monkeypatch.setattr(quotes, 'get_quote_history', mock_get_quote_hist)
+
+   class mock_del_addr():
+      def __init__(self):
+         self.state = 'CA'
+
+   price = quotes.calculate_quote('jim', 1000, mock_del_addr())
+
+   assert price == '240.00'
