5cf6b90555648396150083094dbc7fee9da96d69
Erich McMillan <erich.mcmillan@hp.com>
2020-08-03 10:47:43 -0500

    Potential load hang fix

--- frontend/src/Homepage.js
+++ frontend/src/Homepage.js
@@ -5,34 +5,40 @@ import { LinkContainer } from "react-router-bootstrap";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
 import Routes from './Routes'
 
-export default function Homepage() {
-  return (
-    <div className="Homepage">
-      {/* <div className="lander">
-        <p>Login to access profile. If you are not a member, please register.</p>
-      </div> */}
-        <Navbar fluid collapseOnSelect>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <Link to="/getProfile">Manage Profile</Link>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-          <Nav pullRight>
-            <LinkContainer to = '/getQuote'>
-              <NavItem>Get Quote</NavItem>
-            </LinkContainer>
-            <LinkContainer to = '/history'>
-              <NavItem>Order History</NavItem>
-            </LinkContainer>
-            <LinkContainer to = '/logout'>
-              <NavItem>Logout</NavItem>
-            </LinkContainer>
-            </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-      <Routes />
-    </div>
-  );
+class Homepage extends React.Component {
+
+
+  render() {
+    // alert('homepage render')
+    return (
+      <div className="Homepage">
+        {/* <div className="lander">
+          <p>Login to access profile. If you are not a member, please register.</p>
+        </div> */}
+          <Navbar fluid collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to="/getProfile">Manage Profile</Link>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <LinkContainer to = '/getQuote'>
+                <NavItem>Get Quote</NavItem>
+              </LinkContainer>
+              <LinkContainer to = '/history'>
+                <NavItem>Order History</NavItem>
+              </LinkContainer>
+              <LinkContainer to = '/logout'>
+                <NavItem>Logout</NavItem>
+              </LinkContainer>
+              </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    );
+  }
 }
+
+export default Homepage
